5341825455bb8f0e95508206c9453b6195d1e18d
Sujit-Thapa <sujitthapa.professional@gmail.com>
2025-05-20 23:15:40 +0545

    heropage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
-import './globals.css' 
+import '../globals.css' 
 import { ReactNode } from 'react'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
-        
-        <main >{children}</main>
-    
+      <body className="bg-background">
+        <main>{children}</main>
       </body>
     </html>
   )
