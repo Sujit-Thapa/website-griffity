d7cfad3fb7ddbf06096e3ba85dcaeb4ee9c6ed24
Sujit-Thapa <sujitthapa.professional@gmail.com>
2025-05-21 20:47:53 +0545

    about section

--- components/about.tsx
+++ components/about.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="relative h-screen w-full bg-[#06141b] text-white">
-      {/* Top-left ABOUT */}
+<div className="relative h-screen w-full bg-[#06141b] text-white overflow-hidden">
+  
       <h1 className="absolute text-primary top-10 left-10 text-l font-light z-10">
       [ABOUT GRIFFITY ]
       </h1>
 
-      {/* Centered slogan */}
       <div className="flex justify-center items-center h-full">
         <p className="text-xl text-center z-10 px-4">
           Driven by innovation, creativity and excellence
         </p>
       </div>
 
-      {/* Positioned gradient SVG */}
+    
       <img
         src="/images/gradient.svg"
         alt="gradient"
