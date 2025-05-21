4fd9dfa00ecae176064a93d72f920a700e654b06
shresthasusan <shresthasusan44@gmail.com>
2025-05-21 19:31:11 +0545

    hero and animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import Hero from '../components/hero'
+"use client";
 
+import React, { useEffect, useState } from "react";
+import Hero from "../components/hero";
+import Loader from "../components/loader";
 
-const page = () => {
+const Page = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 30000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main>
-      <Hero/>
-    </main>
-   
-  )
-}
+    <>
+      <Loader />
+      {/* <main>
+        <Hero />
+      </main> */}
+    </>
+  );
+};
 
-export default page
+export default Page;
