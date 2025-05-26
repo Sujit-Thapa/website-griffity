144bda48afea0c3cbe0ee217aad9953f036c381a
shresthasusan <shresthasusan44@gmail.com>
2025-05-26 20:15:13 +0545

    new media query added

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
         lg: "1024px",
         xl: "1280px",
         "2xl": "1440px", // modified 2xl to 1440px
+        "3xl": "1920px", // added 3xl breakpoint
       },
     },
   },
