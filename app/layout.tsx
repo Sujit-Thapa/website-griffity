d9578c3010d27221d4aedc277425dae65d220634
shresthasusan <shresthasusan44@gmail.com>
2025-06-14 19:34:08 +0545

    refactor: update favicon and Open Graph meta tags for improved SEO and browser support

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,21 +8,41 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Griffity Studios</title>
-        {/* Use a standard 32x32 favicon for best results */}
+
+        {/* Favicons */}
         <link
           rel="icon"
           href="/logos/favicon.svg"
           type="image/png"
           sizes="32x32"
         />
-        {/* Optionally add other sizes for better browser support */}
         <link
           rel="icon"
           href="/logos/favicon.svg"
           type="image/png"
           sizes="16x16"
         />
+
+        {/* Open Graph Meta Tags for Link Previews */}
+        <meta property="og:title" content="Griffity Studios" />
+        <meta
+          property="og:description"
+          content="At Griffity, we carry the warmth in everything we create. With the strength of the mountains and the soul of the valleys, we believe design is not just about visuals—it’s about people, feelings, and stories that live in every heart."
+        />
+        <meta
+          property="og:image"
+          content="https://griffitystudios.com/images/logos/bg-logo.svg"
+        />
+        <meta property="og:url" content="https://griffitystudios.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Griffity Studios" />
+
+        <meta
+          name="twitter:image"
+          content="https://griffitystudios.com/logos/og-cover.jpg"
+        />
       </head>
+
       <body
         className={`bg-body ${poppins.className} text-white transition-all ease-in-out duration-700`}
       >
