b508947627f43d35f28f7103b634b32205a84ea8
shresthasusan <shresthasusan44@gmail.com>
2025-06-14 20:05:52 +0545

    re

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <meta property="og:description" content="Evolving mystery" />
         <meta
           property="og:image"
-          content="https://griffitystudios.com/logos/griffity.png"
+          content="https://griffitystudios.com/logos/favicon.svg"
         />
         <meta property="og:url" content="https://griffitystudios.com" />
         <meta property="og:type" content="website" />
